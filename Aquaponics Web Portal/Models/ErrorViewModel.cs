9c6046855f700d28ddec812e2b57b10703f5bdce
João Gonçalves <jmrgs@iscte-iul.pt>
2018-09-27 23:15:56 +0100

    Entity Framework

---
+++ Aquaponics Web Portal/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Aquaponics_Web_Portal.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
